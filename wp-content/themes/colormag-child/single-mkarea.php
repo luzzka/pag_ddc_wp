66c30463e9e7f4c201ff05d62e68bdab35de5609
vane <vmreynagaflores@gmail.com>
2025-08-24 23:11:33 -0500

    fix single-mkArea styles and refactoring

--- wp-content/themes/colormag-child/single-mkarea.php
+++ wp-content/themes/colormag-child/single-mkarea.php
@@ -1,187 +1,237 @@
 <?php
 /**
- * Theme Single Post Section for our theme.
+ * Plantilla para mostrar un post individual del Custom Post Type 'mkarea'.
+ * Este archivo controla la vista de una "Área Funcional" individual.
  *
  * @package ThemeGrill
  * @subpackage ColorMag
  * @since ColorMag 1.0
  */
+
+// Carga el archivo header.php del tema.
+get_header(); ?>
+
+<?php
+do_action( "mkslider_action_display" );
+do_action( 'colormag_before_body_content' );
 ?>
 
-
-<?php get_header(); ?>
-<?php do_action( "mkslider_action_display" ); ?>
-<?php do_action( 'colormag_before_body_content' ); ?>
-<div class="ctnsubtp leftsub">
-   <?php
-      #recupear el objeto
+<div class="main-layout-container">
+   <!-- =================================================================
+      MENÚ DE ÁREAS RELACIONADAS
+      ================================================================= -->
+   <div class="ctnsubtp leftsub">
+      <?php
+      // Obtener el objeto del post actual que se está mostrando en la página (mkarea)
       $mkarea = get_queried_object();
-      $subdireccion = array();
-      if( !empty( $mkarea ) ){
-         #ver si tiene un parent subdireccion
+      $subdireccion = null; 
+
+      // Verificar que el area haya cargado correctamente
+      if ( ! empty( $mkarea ) ) {
+         // el padre del area es la subdirección
          $idsubdireccion = $mkarea->post_parent;
+         // Si tiene un ID de padre, obtenemos el objeto completo de ese post padre
+         if ( $idsubdireccion > 0 ) {
          $subdireccion = get_post( $idsubdireccion );
-      }#end not empty mkarea
-      if( !empty( $subdireccion ) ):
+         }
+      }
+
+      // Si encontramos al padre (una subdirección) se construye el manu de toda la subdir
+      if ( ! empty( $subdireccion ) ) :
+
+         // Definimos los argumentos para una nueva consulta a la base de datos (WP_Query).
          $args = array(
-               "post_type" => "mkarea",
-               "post_per_page" => -1,
-               "post_parent" => $subdireccion->ID,
-               "order" => "ASC",
-               "orderby" => "menu_order");
+            "post_type"      => "mkarea",       // Queremos posts del tipo 'mkarea'.
+            "posts_per_page" => -1,            // traer todos los posts
+            "post_parent"    => $subdireccion->ID, // Solo los que son hijos de la misma subdirección.
+            //"order"          => "ASC",          // Orden ascendente.	
+            "orderby"        => "menu_order"    // Ordenar por el "Orden" definido en el editor de WordPress.
+         );
+
+         // crear un query
          $list_areas = new WP_Query( $args );
       ?>
+
       <div class="listareas clearfix">
          <div class="menu-block-wrapper">
             <ul class="mkmenu">
                <li class="menu-mlid">
-                  <a href="<?php echo get_post_permalink( $subdireccion->ID );  ?>"><?php echo get_the_title($subdireccion->ID); ?></a>
-                  <?php if( $list_areas->have_posts() ) :  ?>
+                  <!-- Enlace al post padre (la subdirección) -->
+                  <a href="<?php echo esc_url( get_permalink( $subdireccion->ID ) ); ?>"><?php echo esc_html( get_the_title( $subdireccion->ID ) ); ?></a>
+
+                  <?php
+                  // mostrar en una lista las areas encontradas
+                  if ( $list_areas->have_posts() ) :
+                  ?>
                      <ul class="menumk">
-                     <?php while( $list_areas->have_posts() ) : $list_areas->the_post(); ?>
-                        <li class="menu-mlid <?php if( get_the_ID() == $mkarea->ID ) echo "active"; ?>">
-                           <a href="<?php the_permalink(); ?>"><?php echo get_the_title( get_the_ID() ); ?></a>
-                        </li>
-                     <?php endwhile;?>
+                     <?php
+                     while ( $list_areas->have_posts() ) : $list_areas->the_post();
+                     // aplicar una clase 'active' si es el area actual
+                     $active_class = ( get_the_ID() == $mkarea->ID ) ? 'active' : '';
+                     ?>
+                     <li class="menu-mlid <?php echo esc_attr( $active_class ); ?>">
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                     </li>
+                     <?php endwhile; ?>
                      </ul>
-                  <?php endif; wp_reset_query(); ?>
+                  <?php
+                  endif;
+                  //  Restaura los datos del post original de la página.
+                  wp_reset_postdata();
+                  ?>
                </li>
             </ul>
          </div>
       </div>
-   <?php endif; ?>
-</div>
-<div class="ctnsubtp centersub">
-  <div id="content" class="clearfix">
-   <?php
-   while ( have_posts() ) : the_post();
-      get_template_part( 'content', 'single' );
-   endwhile;
-   ?>
-   <br><br><br>
-</div><!-- #content -->
-<?php #get_template_part( 'navigation', 'single' ); ?>
-
-<?php if ( get_the_author_meta( 'description' ) ) : ?>
-
-   <div class="author-box">
-
-      <div class="author-img"><?php echo get_avatar( get_the_author_meta( 'user_email' ), '100' ); ?></div>
-
-      <h4 class="author-name"><?php the_author_meta( 'display_name' ); ?></h4>
-
-      <p class="author-description"><?php the_author_meta( 'description' ); ?></p>
-
+      <?php endif; ?>
    </div>
 
-<?php endif; ?>
+   <!-- =================================================================
+      COLUMNA CENTRAL (CENTER SUB) - CONTENIDO PRINCIPAL
+      ================================================================= -->
+   <div class="ctnsubtp centersub">
+      <div id="content" class="clearfix">
+      <?php
+      // muestra el contenido del mkarea actual
+      while ( have_posts() ) : the_post();
+         get_template_part('content', 'single');
+         //the_title( '<h2 class="entry-title">', '</h2>' );
+         //the_content();
 
-<?php if ( get_theme_mod( 'colormag_related_posts_activate', 0 ) == 1 )
-
-get_template_part( 'inc/related-posts' );
-
-?>
-<?php
-
-do_action( 'colormag_before_comments_template' );
-
-         // If comments are open or we have at least one comment, load up the comment template
-
-if ( comments_open() || '0' != get_comments_number() )
-
-   comments_template();
-
-do_action ( 'colormag_after_comments_template' );
-
-?>
-
-</div><!-- #centersub -->
-
-<div class="ctnsubtp rightsub">
-   <?php
-      #recuperar todos las subdirecciones
-   $args = array("numberposts" => 4, "orderby" => "date", "order" => "ASC", "post_type" => "mksubdireccion");
-   $subdirecciones = get_posts( $args );
-   if( !empty( $subdirecciones ) ):
+      endwhile; 
       ?>
-   <div class="block-subdir">
-      <div class="block-inner clearfix">
-         <div class="listsubdir_s">
-            <?php
-            foreach ($subdirecciones as $key => $value) {
-                  #recuperar el color del post
-               $color =  get_post_meta($value->ID, "mkboxcolor", true );
-               $imagen = get_post_meta($value->ID, "mkimagenpost", true );
-               $color = !empty( $color ) ? $color : "FFFFFF";
-               $imagen = !empty( $imagen ) ? $imagen : "";
-               #armar el css
-               $css = ' style="background-color:#'.$color.';';
-               $css .= '" ';
-               ?>
-               <a href="<?php the_permalink($value->ID); ?>">
-                  <div class="itemsubdir_s" <?php echo $css; ?>>
-                     <?php
-                     if( empty( $imagen ) ) {
-                        echo get_the_title($value->ID);
-                     } else {
-                        echo "<img src='".$imagen."' alt='".get_the_title($value->ID)."'>";
-                     }
-                     ?>
-                  </div>
-               </a>
-               <?php
-               }#end foreach subdirecciones
-               ?>
-            </div>
-         </div>
+      <br><br><br>
+   </div><!-- #content -->
+
+   <?php // Si el autor del post tiene una biografía, muestra la caja de autor. ?>
+   <?php if ( get_the_author_meta( 'description' ) ) : ?>
+      <div class="author-box">
+         <div class="author-img"><?php echo get_avatar( get_the_author_meta( 'user_email' ), '100' ); ?></div>
+         <h4 class="author-name"><?php the_author_meta( 'display_name' ); ?></h4>
+         <p class="author-description"><?php the_author_meta( 'description' ); ?></p>
       </div>
    <?php endif; ?>
 
+   <?php 
+   // Si la opción de "posts relacionados" está activa , carga la plantilla correspondiente. 
+   if ( get_theme_mod( 'colormag_related_posts_activate', 0 ) == 1 ) {
+      get_template_part( 'inc/related-posts' );
+   }
+   ?>
+
    <?php
-   #recuperar el side bar de este patrimonio
-   $idenslider = get_post_meta($post->post_parent, 'mkpost_sidebar_iden', true );
-   if( !$idenslider )
-      $idenslider = "0";
-   $slider = array();
-   if( $idenslider != "0" ){
-         #recuperar el slider
-      $options_slider = get_option( "mksidebarsbd", false );
-      if( !empty($options_slider) OR $options_slider != false )
-         $options_slider = stripslashes_deep( unserialize( base64_decode( $options_slider ) ) );
-      $encontrado = false;
-      if( !empty( $options_slider ) && is_array( $options_slider ) )
-         foreach ($options_slider as $key => $value) {
-            if( $idenslider == $key ){
-               $encontrado = true;
-               break;
-            }
-         }#end foreach options_slider
-      if( $encontrado )
-         $slider = $options_slider[$idenslider]['slider'];
-      }
-         if( !empty( $slider ) ):
-         ?>
-         <div class="block-sidebar">
+   // Acción del tema antes de la plantilla de comentarios.
+   do_action( 'colormag_before_comments_template' );
+
+   // Si los comentarios están abiertos o si ya hay al menos un comentario, carga la plantilla de comentarios (comments.php).
+   if ( comments_open() || get_comments_number() != '0' ) {
+      comments_template();
+   }
+
+   // Acción del tema después de la plantilla de comentarios.
+   do_action ( 'colormag_after_comments_template' );
+   ?>
+
+   </div><!-- #centersub -->
+
+   <!-- =================================================================
+      COLUMNA DERECHA (RIGHT SUB) - WIDGETS PERSONALIZADOS
+      ================================================================= -->
+   <div class="ctnsubtp rightsub">
+      <?php
+         // PRIMER BLOQUE: Muestra las subdirecciones
+         $args_subdirs = array(
+            "post_type"      => "mksubdireccion",
+            "posts_per_page" => 4,
+            "orderby"        => "date",
+            "order"          => "ASC"
+         );
+         $subdirecciones = get_posts( $args_subdirs );
+
+         if ( ! empty( $subdirecciones ) ) :
+      ?>
+      <div class="block-subdir">
          <div class="block-inner clearfix">
-            <div class="view-content">
-               <?php foreach ($slider as $key => $slide) {
-                  ?>
-                  <div class="view-item">
-                     <a href="<?php echo $slide['enlace']; ?>">
-                        <div class="item-image">
-                           <img src="<?php echo $slide['imagen'] ?>" alt="<?php echo $slide['titulo'] ?>" class="adaptive-image">
-                        </div>
-                        <div class="field-enlace-menu">
-                           <span class="title"><?php echo $slide['titulo']; ?></span>
-                        </div>
-                     </a>
-                  </div>
-                  <?php
-               } ?>
+            <div class="listsubdir_s">
+               <?php
+               foreach ( $subdirecciones as $subdireccion_item ) {
+                  // Obtenemos los metadatos (campos personalizados) para cada subdirección.
+                  $color = get_post_meta( $subdireccion_item->ID, "mkboxcolor", true );
+                  $imagen = get_post_meta( $subdireccion_item->ID, "mkimagenpost", true );
+
+                  // Asignamos valores por defecto si los campos están vacíos.
+                  $color = ! empty( $color ) ? $color : "FFFFFF";
+                  $imagen = ! empty( $imagen ) ? $imagen : "";
+
+                  // Construimos el CSS en línea de forma segura.
+                  $estilo_css = 'background-color: #' . esc_attr( $color ) . ';';
+               ?>
+                  <a href="<?php echo esc_url( get_permalink( $subdireccion_item->ID ) ); ?>">
+                     <div class="itemsubdir_s" style="<?php echo esc_attr( $estilo_css ); ?>">
+                        <?php
+                        if ( empty( $imagen ) ) {
+                           // Si no hay imagen, muestra el título. Idealmente, siempre muestra la imagen
+                           echo esc_html( get_the_title( $subdireccion_item->ID ) );
+                        } else {
+                           // Si hay imagen, la muestra.
+                           echo '<img src="' . esc_url( $imagen ) . '" alt="' . esc_attr( get_the_title( $subdireccion_item->ID ) ) . '">';
+                        }
+                        ?>
+                     </div>
+                  </a>
+               <?php }  ?>
             </div>
          </div>
       </div>
       <?php endif; ?>
+
+      <?php
+      // SEGUNDO BLOQUE: Muestra un sidebar personalizado 
+      $idenslider = get_post_meta( $post->post_parent, 'mkpost_sidebar_iden', true );
+      $slider = array(); // Inicializamos el array.
+
+      if ( ! empty( $idenslider ) && $idenslider != "0" ) {
+         // Obtener la opción guardada en la tabla wp_options.
+         $options_slider_raw = get_option( "mksidebarsbd", false );
+
+         if ( ! empty( $options_slider_raw ) ) {
+            // Decodifica y deserializa los datos. 
+            $options_slider = unserialize( base64_decode( $options_slider_raw ) );
+
+            // Verificamos que los datos sean un array y que la clave que buscamos exista.
+            if ( is_array( $options_slider ) && isset( $options_slider[$idenslider]['slider'] ) ) {
+               $slider = $options_slider[$idenslider]['slider'];
+            }
+         }
+      }
+
+      // Si logramos obtener los datos del slider, lo mostramos.
+      if ( ! empty( $slider ) && is_array( $slider ) ) :
+      ?>
+         <div class="block-sidebar">
+            <div class="block-inner clearfix">
+               <div class="view-content">
+                  <?php foreach ($slider as $slide) : ?>
+                     <div class="view-item">
+                        <a href="<?php echo esc_url( $slide['enlace'] ); ?>">
+                           <div class="item-image">
+                              <img src="<?php echo esc_url( $slide['imagen'] ); ?>" alt="<?php echo esc_attr( $slide['titulo'] ); ?>" class="adaptive-image">
+                           </div>
+                           <div class="field-enlace-menu">
+                              <span class="title"><?php echo esc_html( $slide['titulo'] ); ?></span>
+                           </div>
+                        </a>
+                     </div>
+                  <?php endforeach; ?>
+               </div>
+            </div>
+         </div>
+      <?php endif; ?>
    </div>
-   <?php do_action( 'colormag_after_body_content' ); ?>
-   <?php get_footer(); ?>
+</div> <!-- .main-layout-container -->
+
+<?php
+// Acción del tema después del contenido principal.
+do_action( 'colormag_after_body_content' );
+get_footer();
+?>
